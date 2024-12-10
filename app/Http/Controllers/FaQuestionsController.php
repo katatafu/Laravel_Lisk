d6e48553ab2f089a1f6b5f5d13f54a05d42b2046
alena_liskova <liskova.aja@gmail.com>
2024-12-10 08:40:59 +0100

    loliksmolik

---
+++ app/Http/Controllers/FaQuestionsController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\FaQuestions;
+use Illuminate\Http\Request;
+
+class FaQuestionsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(FaQuestions $faQuestions)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(FaQuestions $faQuestions)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, FaQuestions $faQuestions)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(FaQuestions $faQuestions)
+    {
+        //
+    }
+}
